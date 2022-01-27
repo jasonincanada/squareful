0623bd01bd9048bb4057f2020ed76f684d26b69c
jasonincanada <nirgle@gmail.com>
2022-01-27 11:45:50 -0500

    first puzzle

---
+++ Squareful.Model/Puzzle.cs
@@ -0,0 +1,17 @@
+﻿namespace Squareful.Model
+{
+    /// <summary>
+    /// A Puzzle is a particular Board and a list of Squares that are shrouded
+    /// </summary>
+    public class Puzzle
+    {
+        public int PuzzleID { get; private set; }
+        public DateTime Published { get; set; }
+
+        // The board doesn't contain much data, so duplicate it within Puzzle to keep things simple
+        public int BoardID { get; set; }
+        public string Sequence { get; set; } = string.Empty;
+
+        public List<Square> Shroud { get; set; } = new();
+    }
+}
